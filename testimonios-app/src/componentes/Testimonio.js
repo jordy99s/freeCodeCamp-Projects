c2055b7f04d3399c1bb1506ecc9b50cea4d7a7f8
Jordy Vargas <jordyvargas182@yahoo.com>
2022-06-15 22:13:39 -0600

    :boom: Se agregan los nuevos testimonios, finalizando el proyecto

--- testimonios-app/src/componentes/Testimonio.js
+++ testimonios-app/src/componentes/Testimonio.js
@@ -2,17 +2,24 @@ import React from "react";
 // incluyo el archivo de estilos
 import '../stylesheets/Testimonio.css';
 // export en esta posicion para exportar mas de un elemento 
-export function Testimonio(){
+// se agrega entre parentesis props para recibir valores y usarlos en componentes
+export function Testimonio(props){
   return(
     <div className="contenedor-testimonio">
       <img 
         className="imagen-testimonio" 
-        src={require("../imagenes/testimonio-emma.png")}
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
         alt="Foto de Emma" />
-      <div className="contenedor-text-testimonio">
-        <p className="nombre-testimonio">Emma Bostian en Suecia</p>
-        <p className="cargo-testimonio">Ingeniera de Sofware en Spotify</p>
-        <p className="texto-testimonio">"I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."</p>
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">
+          <strong>{props.nombre}</strong> en {props.pais}
+        </p>
+        <p className="cargo-testimonio">
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className="texto-testimonio">
+          "{props.testimonio}"
+        </p>
       </div>
     </div>
   );
